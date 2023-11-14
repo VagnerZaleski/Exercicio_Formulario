79cb5da07ae2e3b7b72465831a356198a859e4d5
Vagner Zaleski <z.vagner@hotmail.com>
2023-11-13 21:25:44 -0300

    melhoria na logica

--- main.js
+++ main.js
@@ -2,35 +2,36 @@ const form = document.getElementById('formulario');
 const cmpoA = document.getElementById('n1');
 const cmpoB = document.getElementById('n2');
 
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function (e) {
     e.preventDefault();
 
     const msgSucesso = `Campos preenchidos corretamente! O número ${cmpoA.value} é menor que o número ${cmpoB.value}. `
 
-    if(parseFloat(cmpoA.value) < parseFloat( cmpoB.value)){
+    if (parseFloat(cmpoA.value) < parseFloat(cmpoB.value)) {
         const containerMensagemSucesso = document.getElementById('sucesso-mensagem');
         containerMensagemSucesso.innerHTML = msgSucesso;
         containerMensagemSucesso.style.display = 'block';
         cmpoA.value = '';
         cmpoB.value = '';
 
-    }else{
+    } else {
         n2.style.border = '1px solid red';
         document.getElementById('erro-mensagem').style.display = 'block';
     }
 })
 
-n2.addEventListener('keyup', function(e) {
+
+/*Logica para atualizar os valores dos campos, e remover msg de valido ou invalido */
+n1,n2.addEventListener('keyup', function (e) {
     console.log(e.target.value)
 
-    if (parseFloat(cmpoA.value) < parseFloat( cmpoB.value)) {
-        n2.classList.add('erro-mensagem')
-        document.getElementById('erro-mensagem').style.display = 'block';
-    } else {
-        n2.classList.remove('erro-mensagem')
-        document.getElementById('erro-mensagem').style.display = 'none';
-    }
+    const containerMensagemSucesso = document.getElementById('sucesso-mensagem');
+    containerMensagemSucesso.style.display = 'none';
+    document.getElementById('erro-mensagem').style.display = 'none';
+
+    
 });
+
 
 
 
